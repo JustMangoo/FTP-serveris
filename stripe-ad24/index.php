4a1791253d4eb031f75db3373c9dbda8095856e4
Aleksis <daugatsa@gmail.com>
2024-01-29 11:39:38 +0200

    success style

--- stripe-ad24/index.php
+++ stripe-ad24/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stripe apmaksa</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="https://cdn.iconscout.com/icon/free/png-256/free-stripe-2-498440.png" type="image/x-icon">
 </head>
 <body>
     <div class="container">
@@ -19,10 +19,10 @@
                 <button type="submit">Ielogoties</button>
             </form>
         </div>
+
         <div class="right-panel">
             <h1>Iegūt pieeju</h1>
-            <p>Sistēma piejama tikai reģistrētiem lietotājiem kri iegādājušies šo pakalpjumu. Lai iegūtu pieeju,
-                nospiediet uz zemāk redzemās pogas un veiciet apmaksu, pēc kuras veikšanas saņēmsjer pieejas paroli</p>
+            <p>Sistēma pieejama tikai reģistrētiem lietotājiem, kuri iegādājušies šo pakalpojumu. Lai iegūtu pieeju, nospiediet uz zemāk redzamās pogas un veiciet apmaksu, pēc kuras veikšanas saņemsiet pieejas paroli!</p>
             <p>Cena: <b>9.99 EUR</b></p>
             <form action="checkout.php" method="post">
                 <button class="stripe-button">Iegādāties pieeju</button>
--- stripe-ad24/success.php
+++ stripe-ad24/success.php
@@ -5,30 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stripe apmaksa</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="https://cdn.iconscout.com/icon/free/png-256/free-stripe-2-498440.png" type="image/x-icon">
 </head>
 <body>
 
-    <?php 
-        if(!empty($_GET['session_id'])) {
+    <?php
+        if(!empty($_GET['session_id'])){
             $session_id = $_GET['session_id'];
             require_once 'stripe-php/init.php';
             require_once 'config.php';
 
-            try {
+            try{
                 $checkout_session = \Stripe\Checkout\Session::retrieve($session_id);
             }catch(Exception $e){
                 $api_error = $e->getMessage();
             }
 
-            if (empty($api_error) && $checkout_session ) {
+            if(empty($api_error) && $checkout_session){
                 $customer_email = $checkout_session->customer_details->email;
-                echo $customer_email;
 
                 try{
-                    $paymentIntent = \Stripe\PaymentIntent::retrieve($checkout_session->$payment_intent);
-                    ($checkout_session->$payment_intent);
-                }catch(\Stripe\Exception\ApiErrorException $e) {
+                    $paymentIntent = \Stripe\PaymentIntent::retrieve($checkout_session->payment_intent);
+                }catch(\Stripe\Exception\ApiErrorException $e){
                     $api_error = $e->getMessage();
                 }
 
@@ -36,28 +34,41 @@
                     if(!empty($paymentIntent) && $paymentIntent->status == "succeeded"){
                         $transactionID = $paymentIntent->id;
                         $payment_status = $paymentIntent->status;
-                        //parole
+                        $password = "";
+                        for($i = 0; $i < 4; $i++){
+                            $password .= chr(rand(65, 90));
+                            $password .= chr(rand(48, 57));
+                        }
 
-                        $statusMsg = "Maksajums veiksmigi apstrādāts";
+                        $password .= chr(rand(35, 37));
+
+                        $statusMsg = "Maksājums veiksmīgi apstrādāts!";
                         $dataMsg = "
-                        <p>Maksajuma ID: <b>$transactionID</b></p>
-                        <p>Maksajuma statuss: <b>$payment_status</b></p>
-                        <p>E-pasts: <b>$customer_email</b></p>
-                        <p>Parole: <b></b></p>
+                            <p>Pieejas parole ir nosūtīta uz jūsu e-pastu!</p>
+                            <p>Ja parole nav saņemta - sazinies ar mums pa tālruni: 29 999 999.</p>
+                            <p>Maksājuma ID: <b>$transactionID</b></p>
+                            <p>Maksājuma statuss: <b>$payment_status</b></p>
+                            <p>E-pasts: <b>$customer_email</b></p>
+                            <p>Parole: <b>$password</b></p>
+                            <button type='submit' class='stripe-button'>Ielogoties</button>
                         ";
                     }
                 }else{
-                    $statusMsg = "Nav iespejams iegut maksajuma informaciju!";
+                    $statusMsg = "Nav iespējams iegūt maksājuma informāciju!";
                 }
             }else{
-                $statusMsg = "Neeksistejoss maskajums";
-                header("location: ./");
+                $statusMsg = "Neeksistējošs maksājums!";
             }
+
+        }else{
+            $statusMsg = "Neeksistējošs maksājums!";
+            header("location: ./");
         }
     ?>
     <div class="container">
         <div class="panel">
             <h1><?php echo $statusMsg; ?></h1>
+            <?php echo $dataMsg; ?>
         </div>
     </div>
 </body>
